c83dba0595082b0c2ad7cee50492701585d2651d
Tomer Elmalem <telmalem@gmail.com>
2012-10-07 19:15:23 -0400

    Add edit/update/destroy actions

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -1,6 +1,7 @@
 class DinnersController < ApplicationController
   def index
-    @dinner = Dinner.all
+    @dinners = Dinner.all
+    @dinner  = @dinners[rand(@dinners.size)]
   end
 
   def new
@@ -10,6 +11,22 @@ class DinnersController < ApplicationController
   def create
     @dinner = Dinner.new(params[:dinner])
     @dinner.save
+    redirect_to dinners_path
+  end
+
+  def edit
+    @dinner = Dinner.find(params[:id])
+  end
+
+  def update
+    @dinner = Dinner.find(params[:id])
+    @dinner.update_attributes(params[:dinner])
+    redirect_to dinners_path
+  end
+
+  def destroy
+    @dinner = Dinner.find(params[:id])
+    @dinner.destroy
     redirect_to dinners_path
   end
 end
