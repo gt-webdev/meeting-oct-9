750f54f885c87b5de9ecb2e6cc8bcbef2c780f19
Tomer Elmalem <telmalem@gmail.com>
2012-10-07 14:44:47 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtf2eat::Application.config.secret_token = 'f89b09634635d03ea6d302c765c6c3743f0cc1a7646619b89f9d9330734fc0deb936d874c1a20b3032ee0b57e9f2116ed67b53922f5a6ac38251a08c1b9f3e14'
